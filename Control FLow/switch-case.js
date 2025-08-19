0992b0310f31b5fd173d10ec872a3eb8f3f60157
tanjimtaosif <itsmahtab007@gmail.com>
2025-08-19 16:55:23 +0600

    Simple switch case example in js

--- Control FLow/switch-case.js
+++ Control FLow/switch-case.js
@@ -1,9 +1,19 @@
+// case 1: → If val === 1, run this block (currently empty).
+// case 2: → If val === 2, run this block (currently empty).
+// case 3: → If val === 3, run this block (currently empty).
+// default: → Runs if val doesn’t match 1, 2, or 3.
+
+let val = 2;
 switch (val) {
     case 1:
+        console.log("Value is One");
         break;
     case 2:
+        console.log("Value is Two");
         break;
     case 3:
+        console.log("Value is Three");
         break;
     default:
+        console.log("Value not found");
 }
