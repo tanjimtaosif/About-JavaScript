87bd026517b6671bc294b5b578a21cca50c11a02
tanjimtaosif <itsmahtab007@gmail.com>
2025-08-19 15:33:07 +0600

    Truthy and Falsy in js

---
+++ Data Types & Type System/truthyAndFalsy.js
@@ -0,0 +1,12 @@
+// Truthy and Falsy
+// 0, false, "", null, undefined, NaN, document.all -> Falsy
+// Other are Truthy
+
+let a = 0;
+
+if (a){
+    console.log("Truthy");    
+}
+else{
+    console.log("Falsy");
+}
