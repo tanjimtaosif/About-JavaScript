a1c89a2b3299cd7121d6c79c60da53ff0db472b4
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-06 18:47:17 +0600

    Dynamic type in js

---
+++ Data Types/dynamic_typing.js
@@ -0,0 +1,9 @@
+// Dynamic typing ---> There is not static data types in Js. Here Dynamic typing is used. So, in a signle variable we can use multiple type of data.
+
+let a = 12;
+a = true;
+a = 'Mahtab';
+a = {};
+a = [1,2,3];
+a = null;
+a = undefined;
