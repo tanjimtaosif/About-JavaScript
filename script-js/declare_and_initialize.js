8aebf0d84267a9960e062b05b91ef3056d7c1867
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-06 14:51:53 +0600

    Variable declare and initialization

---
+++ script-js/declare_and_initialize.js
@@ -0,0 +1,10 @@
+// var let const
+
+var a; // Declare variable
+var a = 12; // Declare and initialization
+
+let a; // Declare variable
+let a = 12; // Declare and initialization 
+
+// const a; -> 'const' declarations must be initialized.
+const a = 12;
