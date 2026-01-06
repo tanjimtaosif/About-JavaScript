0b09b8bb37d6b5b13525986e25c35fa217280300
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-06 16:16:44 +0600

    Scope in the JavaScript

--- script-js/scope.js
+++ script-js/scope.js
@@ -29,10 +29,13 @@ function varInFunction() {
 function letExample() {
     if (true) {
         let blockScoped = "I am trapped in this IF statement";
+        var functionalScoped = "I am not trapped in this IF statement";
         console.log(blockScoped); // Works
     }
     // console.log(blockScoped); // Error: blockScoped is not defined here
+    console.log(functionalScoped); // Wroks
 }
+// letExample(); run the function to see the result 
 
 /**
  * 4. CONST (Block Scope + Immutability)
