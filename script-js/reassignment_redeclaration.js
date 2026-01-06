9af6299212b17818acc5be20ab893404f8f30ff1
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-06 15:33:29 +0600

    Variable Declaration, Initialization and Re-Declaration in JavaScript

---
+++ script-js/reassignment_redeclaration.js
@@ -0,0 +1,10 @@
+var a; // Variable declaration
+var a = 13; // Initialize
+a = 32; // Re-Assignment
+
+var a = 34; // Re-Declare
+
+let b = 12; // Initialize
+b = 34; // Re-Assignment
+
+// let b = 12; let dont allow  re-declaring
