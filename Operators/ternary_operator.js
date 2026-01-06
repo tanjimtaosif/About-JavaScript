bdb414885f04fa55e4693619becd5a338e24c9b4
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-06 21:33:16 +0600

    Ternary operator in JavaScript

---
+++ Operators/ternary_operator.js
@@ -0,0 +1,11 @@
+// ?:
+
+// Condition ? If the condition is ture  : If not then this
+// 12 > 15 ? console.log("true") : console.log("false");
+
+let points = 20;
+let status = points > 100 ? "Gold" : points > 50 ? "Silver" : "Bronze";
+
+console.log(status);
+
+
