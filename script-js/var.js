f889ff8a10d97bd2bc4d0a13de7334cda068026f
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-06 14:58:48 +0600

    About var

---
+++ script-js/var.js
@@ -0,0 +1,7 @@
+var a = 12; 
+var a = 40;
+// when create variable with var it add itself to the window
+// var is function scoped
+// we can redeclare variables created with var without any error with same variable name
+
+console.log(a);
