09e50df2b840acf5635970e462bb4ba92e5bd85a
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-06 22:37:40 +0600

    Switch case in JavaScript

---
+++ Control Flow/switch_case.js
@@ -0,0 +1,12 @@
+let day = "Monday";
+
+switch (day) {
+    case "Monday":
+        console.log("Start of the work week!");
+        break; // Stop here if matched
+    case "Friday":
+        console.log("Weekend is near!");
+        break;
+    default:
+        console.log("Just a normal day.");
+}
