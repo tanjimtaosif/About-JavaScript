3ab39c78a7aeb485995ae021472f3defb8b26b9b
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-07 00:16:10 +0600

    Loop in JavaScript

---
+++ Loops/script.js
@@ -0,0 +1,15 @@
+// for
+// while
+// do-while
+// for-in
+// for-of
+// for-each
+// Where to start , Where to go, How to go. - for loop
+// 1 to 40 -> for loop
+// 20 to 60 -> for loop
+
+// Where to start , Where to stop , How to go. - while loop
+// Hello to Bye -> While loop
+
+
+
