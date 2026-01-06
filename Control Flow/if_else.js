06a18894747aefbfe57301b23f7673ae6881264d
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-06 22:37:28 +0600

    If else statement in JavaScript

---
+++ Control Flow/if_else.js
@@ -0,0 +1,7 @@
+let isRaining = true;
+
+if (isRaining) {
+    console.log("Take an umbrella!");
+} else {
+    console.log("Enjoy the sun!");
+}
---
+++ Control Flow/if_else_if.js
@@ -0,0 +1,9 @@
+let time = 14; // 2 PM
+
+if (time < 12) {
+    console.log("Good Morning");
+} else if (time < 18) {
+    console.log("Good Afternoon"); // This runs
+} else {
+    console.log("Good Evening");
+}
