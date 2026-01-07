b347481f38fd9adf562f0b8cd3a6f11e78486d3d
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-07 19:58:19 +0600

    Hoisting in function declaration and expression

---
+++ Functions/Hoisting differences between declaration and expression/hoisting_function_declaration.js
@@ -0,0 +1,9 @@
+abcd();
+
+
+
+
+// Here function get hoisted and run without any error
+let abcd = function () {
+    console.log("heyehey");
+}
---
+++ Functions/Hoisting differences between declaration and expression/hoisting_function_expression.js
@@ -0,0 +1,7 @@
+abc();
+
+//  Here function won't run will give error fucntion can't be accessed before initialization
+
+let abc = function () {
+    console.log('Hello!!!');
+}
