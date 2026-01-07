0cc5f484e9ee2a697fc36885d5580fc4685757bb
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-07 21:13:35 +0600

    Arrow function in JavaScript

--- Functions/Function declarations, expressions, and arrow functions/arrow_function.js
+++ Functions/Function declarations, expressions, and arrow functions/arrow_function.js
@@ -1,4 +1,5 @@
-// fat arrow func
+// fat arrow function
+// "We shouldn't use arrow functions for object methods because they don't have their own this. This means they can't access other properties inside the same object."
 let fnc = () => {
     console.log("Hello There");
 }
