d1b65dfc78ccf6eba67ab4f1e4963b8d0045b787
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-08 20:20:36 +0600

    Nested object and deep access

---
+++ Objects/nesting_and_deep_ccess.js
@@ -0,0 +1,15 @@
+const user = {
+    name: "Tanjim",
+    address: {
+        city: "Dhaka",
+        pin: 1229,
+        location: {
+            lat: 23.2,
+            lng: 77.4,
+
+        },
+
+    },
+};
+
+console.log(user.address.location.lng);
