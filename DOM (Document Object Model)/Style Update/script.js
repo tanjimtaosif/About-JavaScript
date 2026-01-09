a4337f3b4266a5f4f0b8f7ec1e70fee5b737de9b
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-09 19:48:33 +0600

    Updating style using DOM

---
+++ DOM (Document Object Model)/Style Update/script.js
@@ -0,0 +1,6 @@
+let h1 = document.querySelector("h1");
+h1.classList.add("lol");
+// h1.classList.toggle("lol"); // what it does is if the class was added it removes if not it adds the class.
+h1.classList.remove("col");
+
+console.dir(h1);
---
+++ DOM (Document Object Model)/Style Update/style_by.js
@@ -0,0 +1,7 @@
+let h1 = document.querySelector("h1")
+h1.style.color = "red";
+h1.style.backgroundColor = "black";
+h1.style.fontFamily = "sans-serif";
+h1.style.textTransform = "capitalize";
+
+console.dir(h1);
