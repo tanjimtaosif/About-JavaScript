b6d3a571f3a9799c4a13ff14d50c79e921836d97
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-09 19:38:26 +0600

    Selecting elements in JavaScript

---
+++ DOM (Document Object Model)/Selecting elements/getElementById and getElementsByClassName/script.js
@@ -0,0 +1,16 @@
+// DOM Manipulation
+// selecting html element 
+// text changing
+// changing html
+// changing css
+// changing attribute
+// adding event listener
+
+let a = document.getElementById("abc");
+let b = document.getElementsByClassName("hi");
+console.log(a);
+// when you get nodes as tag then use .dir then we can open the node
+console.dir(a);
+console.log(b);
+
+
