1da3e32e9ff60396b98e553088548c5ff4875f30
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-09 19:37:42 +0600

    Text and Content access in JavaScript

---
+++ DOM (Document Object Model)/Text or content access/script.js
@@ -0,0 +1,9 @@
+let change = document.querySelector('h1');
+
+// We use innerHTML to manipulate the html
+change.innerHTML = "<i>Hello Tanjim how are you?</i>"
+
+// And these two to manipulate the inner content of the html
+// change.textContent = "Hi Mahtab How are you?";
+// change.innerText = "Hi Taosif How are you?";
+
