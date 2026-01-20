05e8772e39cabcb134bffd125095d00a89a9469b
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-20 21:20:20 +0600

    Add new practice DOM script and update existing script for clarity

---
+++ DOM (Document Object Model)/Simple Questions and Problem in DOM/practice_dom_11.js
@@ -0,0 +1,2 @@
+let h1 = document.createElement("h1")
+console.log(h1);
--- DOM (Document Object Model)/Text or content access/script.js
+++ DOM (Document Object Model)/Text or content access/script.js
@@ -2,7 +2,7 @@ let change = document.querySelector('h1');
 
 // We use innerHTML to manipulate the html
 change.innerHTML = "<i>Hello Tanjim how are you?</i>"
-
+console.dir(change);
 // And these two to manipulate the inner content of the html
 // change.textContent = "Hi Mahtab How are you?";
 // change.innerText = "Hi Taosif How are you?";
