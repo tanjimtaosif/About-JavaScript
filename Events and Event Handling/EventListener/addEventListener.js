bdcbcfe49cddf6b8b955253268e25abb04a5ef2a
tanjimtaosif <itsmahtab007@gmail.com>
2026-01-21 19:27:52 +0600

    Add HTML and JavaScript files for click and double-click event handling

---
+++ Events and Event Handling/EventListener/addEventListener.js
@@ -0,0 +1,8 @@
+let a = document.querySelector("h1");
+let b = document.querySelector("h2");
+a.addEventListener("click", function(){
+    a.style.color = "red";
+});
+b.addEventListener("dblclick", function(){
+    b.style.color = "green";
+});
