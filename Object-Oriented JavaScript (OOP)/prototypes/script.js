c78fd2bf86c1026051f54579fa856fd1a225d9fb
tanjimtaosif <itsmahtab007@gmail.com>
2026-02-22 12:45:40 +0600

    Add initial implementation of CreatePencil class with write method and instances

---
+++ Object-Oriented JavaScript (OOP)/prototypes/script.js
@@ -0,0 +1,17 @@
+function CreatePencil(name, price, color, company) {
+  this.name = name;
+  this.price = price;
+  this.color = color;
+  this.company = company;
+}
+
+CreatePencil.prototype.write = function (text) {
+  let h1 = document.createElement("h1");
+  h1.textContent = text;
+  h1.style.color = this.color;
+  console.log(h1);
+  document.body.append(h1);
+};
+
+let pencil1 = new CreatePencil("Nataraj", 10, "black", "nataraj");
+let pencil2 = new CreatePencil("Doms", 10, "red", "doms");
