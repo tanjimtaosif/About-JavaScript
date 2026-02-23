9a856f282a67cfaae33278420d14cd5ff6989314
tanjimtaosif <itsmahtab007@gmail.com>
2026-02-23 21:14:56 +0600

    Add throttling implementation with input event handling

---
+++ Performance Optimization/Throttling/throttling.js
@@ -0,0 +1,20 @@
+let input = document.querySelector("input");
+
+function throttle(fnc, delay) {
+  let timer = 0;
+  return function (...args) {
+    let now = Date.now();
+
+    if (now - timer >= delay) {
+      timer = now;
+      fnc(...args);
+    }
+  };
+}
+
+input.addEventListener(
+  "input",
+  throttle(function () {
+    console.log("Hey! We are learning Throttling!");
+  }, 1000),
+);
