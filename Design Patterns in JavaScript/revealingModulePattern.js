a76ee74110bbac9eb12244c714e553b858483c1a
tanjimtaosif <itsmahtab007@gmail.com>
2026-02-23 16:39:06 +0600

    Add revealing module pattern implementation for bank operations

---
+++ Design Patterns in JavaScript/revealingModulePattern.js
@@ -0,0 +1,27 @@
+// Revealing Module Pattern
+// IIFE - Immediently Invoked Function Expression
+
+let Bank = (function () {
+  let bankbalance = 12000;
+
+  function checkBalance() {
+    console.log(bankbalance);
+  }
+
+  function setBalance(val) {
+    bankbalance = val;
+  }
+
+  function withdraw(val) {
+    if (val <= bankbalance) {
+      bankbalance -= val;
+      console.log(bankbalance);
+    }
+  }
+
+  return {
+    check: checkBalance,
+    set: setBalance,
+    draw: withdraw,
+  };
+})();
